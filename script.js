f69f8a217f13b277c59c19b74dc6a9ead00d2021
Noe <noelia.pedrares@gmail.com>
2023-06-15 09:01:48 +0100

    Añadir función 'buscar cocktail'

--- script.js
+++ script.js
@@ -16,3 +16,34 @@ function insertarCocktail() {
    
 });
 }
+
+document.getElementById('buscar').addEventListener('click', buscarCocktail);
+function buscarCocktail(){
+   let nombre = document.getElementById('inputBuscar').value;
+   fetch('https://localhost:7234/api/cocktail/buscar/' + nombre,
+   {
+      method: 'GET',
+   })
+   .then((response) => response.json())
+   .then((data) => leerFora(data) );
+
+}
+function leerFora(data) {
+    document.getElementById('cockteles').innerHTML = '';
+    let nombre, instruction, foto, id;
+    let div = document.createElement('div');
+    div.className = 'individual';
+    document.getElementById('cockteles').appendChild(div);
+    nombre = document.createElement('h2');
+    nombre.innerHTML = data.nombre;
+    div.appendChild(nombre);
+    id = document.createElement('h5');
+    id.innerHTML = `Id del cocktail: ${data.id}`;
+    div.appendChild(id);
+    instruction = document.createElement('p');
+    instruction.innerHTML = data.receta;
+    div.appendChild(instruction);
+    foto = document.createElement('img');
+    foto.src = data.fotoSrc;
+    div.appendChild(foto);   
+ };
